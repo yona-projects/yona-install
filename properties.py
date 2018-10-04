aef0a268a1762b551e46d12d59730b01e3676e2d
이지호 <search5@gmail.com>
2018-10-04 23:24:05 +0900

    설정파일 수정 오류 해결

--- properties.py
+++ properties.py
@@ -4,11 +4,11 @@ def db_settings(application_conf, setting):
 
         for line_no, line in enumerate(content.copy()):
             if line.startswith("# MariaDB"):
-                del content[line_no:line_no + 3]
+                del content[line_no + 1:line_no + 5]
 
-                content.insert(line_no, "db.default.driver = org.mariadb.jdbc.Driver")
-                content.insert(line_no + 1, "jdbc:mariadb://{host}:{port}/{name}?useServerPrepStmts=true".format_map(setting))
-                content.insert(line_no + 2, "db.default.user = {0}".format(setting['user']))
-                content.insert(line_no + 3, "db.default.password = {0}".format(setting['passwd']))
+                content.insert(line_no + 1, "db.default.driver = org.mariadb.jdbc.Driver\n")
+                content.insert(line_no + 2, "db.default.url = jdbc:mariadb://{host}:{port}/{name}?useServerPrepStmts=true\n".format_map(setting))
+                content.insert(line_no + 3, "db.default.user = {0}\n".format(setting['user']))
+                content.insert(line_no + 4, "db.default.password = {0}\n".format(setting['passwd']))
 
         return content
