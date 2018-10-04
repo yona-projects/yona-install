87f8d9fc13c8f26871655a5cbad0ed9d429ebff3
이지호 <search5@gmail.com>
2018-10-05 00:28:11 +0900

    default.url 값을 따옴표로 감쌈

--- properties.py
+++ properties.py
@@ -1,4 +1,6 @@
 def db_settings(application_conf, setting):
+    db_url = "db.default.url = \"jdbc:mariadb://{host}:{port}/{name}?useServerPrepStmts=true\"\n"
+
     with open(str(application_conf.resolve()), 'r') as conf:
         content = conf.readlines()
 
@@ -7,7 +9,7 @@ def db_settings(application_conf, setting):
                 del content[line_no + 1:line_no + 5]
 
                 content.insert(line_no + 1, "db.default.driver = org.mariadb.jdbc.Driver\n")
-                content.insert(line_no + 2, "db.default.url = jdbc:mariadb://{host}:{port}/{name}?useServerPrepStmts=true\n".format_map(setting))
+                content.insert(line_no + 2, db_url.format_map(setting))
                 content.insert(line_no + 3, "db.default.user = {0}\n".format(setting['user']))
                 content.insert(line_no + 4, "db.default.password = {0}\n".format(setting['passwd']))
 
